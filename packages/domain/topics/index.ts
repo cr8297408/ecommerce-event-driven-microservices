8e931bd05d6ca78afdaef35bd32f5ab5cf5e6ae6
cesar <cr8297408@gmail.com>
2025-12-22 22:04:14 -0500

    feat: update Topics enum with additional product and inventory failure events

--- packages/domain/topics/index.ts
+++ packages/domain/topics/index.ts
@@ -5,8 +5,10 @@ export enum Topics {
   PRODUCT_CREATION_FAILED='product.creation.failed',
   UPDATE_PRODUCT='product.update',
   PRODUCT_UPDATED='product.updated',
+  PRODUCT_UPDATED_FAILED='product.update.failed',
   DELETE_PRODUCT='product.delete',
   PRODUCT_DELETED='product.deleted',
+  PRODUCT_DELETION_FAILED='product.deletion.failed',
 
   // Inventory
   RESERVE_STOCK='inventory.reserve',
@@ -14,10 +16,12 @@ export enum Topics {
   STOCK_RESERVATION_FAILED='inventory.reservation.failed',
   RELEASE_STOCK='inventory.release',
   STOCK_RELEASED='inventory.released',
+  STOCK_RELEASE_FAILED='inventory.release.failed',
 
   // Orders/Checkout
   CREATE_ORDER='order.create',
   ORDER_CREATED='order.created',
+  ORDER_CREATION_FAILED='order.creation.failed',
   ORDER_CONFIRMED='order.confirmed',
   ORDER_CANCELLED='order.cancelled',
 
